313850606109801d3cb4a6752c0a3bf27da3617f
alexlai18 <104405034+alexlai18@users.noreply.github.com>
2023-01-20 19:30:12 +1100

    Add files via upload

---
+++ src/main/java/dungeonmania/interfaces/ally/Ally.java
@@ -0,0 +1,10 @@
+package dungeonmania.interfaces.ally;
+
+
+public interface Ally {
+
+    public abstract boolean getMindControlled(); // returns true if the ally is mind controlled
+    
+    public abstract boolean getBribed(); // returns true if the ally is bribed
+    
+}
